01d5c54212636c66f52ab776deef5ba29012411d
caballero03 <keith@youngbloodtech.com>
2019-05-24 17:55:25 -0700

    A few more changes.

--- src/main.php
+++ src/main.php
@@ -9,12 +9,11 @@ $container = require __DIR__ . '/../config/container.php';
 
 use Micro\Model\Test\Test;
 
+
 $test = new Test($container);
 
 while(1) {
     sleep(1);
-    
-//    echo 'Running: ' . date('H:i:s') . PHP_EOL;
 
     $test->test();
 }
